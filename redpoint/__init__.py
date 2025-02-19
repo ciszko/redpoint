3a0b2f87cdaacefced14e15e25208d48972a6372
ciszko <marcinkalinski1997@gmail.com>
2025-02-19 21:10:28 +0100

    fix ruff

--- redpoint/__init__.py
+++ redpoint/__init__.py
@@ -1,2 +1,3 @@
 from .grading import Grade, GradingSystem, UnknownGrade, UnknownSystem, ConversionError
-from .mappings import *
+
+__all__ = ["Grade", "GradingSystem", "UnknownGrade", "UnknownSystem", "ConversionError"]
